18f7a523c3f5431693701b36437c026f7f74d78a
Christopher Woodall <chris.j.woodall@gmail.com>
2011-05-25 17:32:35 -0400

    Initial Commit

---
+++ LittleCalc/LittleCalcAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  LittleCalcAppDelegate.h
+//  LittleCalc
+//
+//  Created by Christopher Woodall on 5/25/11.
+//  Copyright 2011 Boston University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LittleCalcAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ LittleCalc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LittleCalc
+//
+//  Created by Christopher Woodall on 5/25/11.
+//  Copyright 2011 Boston University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
